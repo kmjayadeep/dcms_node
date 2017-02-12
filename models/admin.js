e3748d06f97121d51cab45bd4c1f9de2094affd2
Jayadeep KM <kmjayadeep@gmail.com>
2017-02-12 11:48:54 +0530

    deleted workshop model

--- models/admin.js
+++ models/admin.js
@@ -28,8 +28,12 @@ module.exports = function(sequelize, DataTypes) {
         Status codes
         ------------
         0 - not verified
-        10 - superadmin
-
+        10 - superadmin - has all privilages
+        9 - admin - cannot add/edit other admins
+        8 - event admin - can edit their event and see details about it
+        7 - registration desk- can view registered students and corresponding events, can verify payments by scanning qr code
+        6 - hospitality - view accomodation details
+        5 - event volunteer - can check whether a student is eligible to enter an event and mark attendance
         */
 
     }, {
