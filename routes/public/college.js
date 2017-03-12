a00490b94b16e0ed0ffb862130950606cc10433a
Nisham <moonblade168@gmail.com>
2017-03-12 13:54:14 +0530

    public add college

--- routes/public/college.js
+++ routes/public/college.js
@@ -36,4 +36,31 @@ router.get('/', (req, res, next) => {
         });
 });
 
+/**
+ * @api {put} /public/college/ add college
+ * @apiDescription add a new college to server
+ * @apiVersion 0.2.0
+ * @apiGroup Admin
+ * @apiParam {string} name college name
+ * @apiSuccessExample success
+ * {
+  "id": 8,
+  "name": "New College",
+  "updatedAt": "2017-03-11T18:28:33.000Z",
+  "createdAt": "2017-03-11T18:28:33.000Z"
+}
+
+    @apiErrorExample error
+    {"code":22,"message":"Could not put college"}
+ */
+router.put('/', (req, res, next) => {
+    models.college.create(req.body)
+        .then(result => {
+            return res.json(result);
+        }).catch(error => {
+            constant.cantPutCollege.data = error;
+            return res.status(400).json(constant.cantPutCollege);
+        })
+});
+
 module.exports = router;
